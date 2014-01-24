7821d974785f77a00e41c2289536f1fe19c521cf
Austin Dubina <austin.dubina@jivesoftware.com>
2014-01-23 23:36:44 -0800

    added ability to import text file and parse string into integer array. Declaring an 10000 integer array proved to be too demanding for IDE

--- project1/src/com/company/Main.java
+++ project1/src/com/company/Main.java
@@ -1,15 +1,55 @@
 package com.company;
 
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        int[] fullArray = new int[]{22, -27, 38, -34, 49, 40, 13, -44, -13, 28, 46, 7, -26, 42, 29, 0, -6, 35, 23, -37,
-                               10, 12, -2, 18, -12, -49, -10, 37, -5, 17, 6, -11, -22, -17, -50, -40, 44, 14, -41, 19,
-                               -15, 45, -23, 48, -1, -39, -46, 15, 3, -32, -29, -48, -19, 27, -33, -8, 11, 21, -43, 24,
-                               5, 34, -36, -9, 16, -31, -7, -24, -47, -14, -16, -18, 39, -30, 33, -45, -38, 41, -3, 4,
-                               -25, 20, -35, 32, 26, 47, 2, -4, 8, 9, 31, -28, 36, 1, -21, 30, 43, 25, -20, -42};
+    public static void main(String[] args) throws IOException {
 
         //first algorithm
+        String arrayString = null;
+
+
+        if (args.length <= 0) {
+            System.out.println("please specify the full path of the .txt file you wish you examine");
+        }
+        else {
+            try {
+                File file = new File(args[0]);
+                FileInputStream fileInputStream =  new FileInputStream(file);
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+                DataInputStream dataInputStream = new DataInputStream(bufferedInputStream);
+
+                while (dataInputStream.available() != 0) {
+                    arrayString = dataInputStream.readLine();
+                }
+
+                fileInputStream.close();
+                bufferedInputStream.close();
+                dataInputStream.close();
+            }
+            catch (FileNotFoundException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String[] items = arrayString.replaceAll("\\[", "").replaceAll("\\]", "").split(",");
+
+            int[] fullArray = new int[items.length];
+
+            for (int i = 0; i < items.length; i++) {
+                try {
+                    fullArray[i] = Integer.parseInt(items[i]);
+                } catch (NumberFormatException nfe) {};
+            }
+            System.out.println(algorithm1(fullArray));
+            System.out.println(algorithm2(fullArray));
+        }
+    }
+
+    static public int algorithm1(int[] fullArray) {
         int beginningIndex;
         int endingIndex;
         int subArraySum;
@@ -34,9 +74,13 @@ public class Main {
             }
             beginningIndex++;
         }
-        System.out.println(finalSum);
+        return finalSum;
+    }
 
+    static public int algorithm2(int[] fullArray) {
         //Second Algorithm
+        int subArraySum;
+        int finalSum;
         finalSum = fullArray[0];
         for (int i = 0; i < fullArray.length; i++) {
             subArraySum = 0;
@@ -48,6 +92,6 @@ public class Main {
                 }
             }
         }
-        System.out.println(finalSum);
+        return finalSum;
     }
 }
