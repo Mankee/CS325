feb66812aa8cf33ae59966c37f61f9fe5134e12b
Austin Dubina <austin.dubina@jivesoftware.com>
2014-02-05 13:28:12 -0800

    uploaded project 1 source code to project two folder. Project 2 will be similar to divide and conqure from project 1

---
+++ project2/ma.h
@@ -0,0 +1,18 @@
+/* Project: #1 MaxSubArray
+ *
+ * Team RADZ:
+ *  Rittie Chuaprasert (chuaprar)
+ *  Austin Dubina (weila)
+ *  Darren Baker (bakerdar)
+ *  Zac Knudson (knudsoza)
+ * 
+ * CS 325-400
+ * Winter 2014
+ * 
+*/
+
+#include <stdio.h>
+
+int a1_maxsubarray(int a_size, int a[]);
+int a2_maxsubarray(int a_size, int a[]);
+int a3_maxsubarray(int a_size, int a[]);
