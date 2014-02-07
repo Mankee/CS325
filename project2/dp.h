faed18eef31c4f850c4d392c314f19280f7b6159
BAKERDAR <bakerdar@onid.oregonstate.edu>
2014-02-07 14:27:33 -0800

    Create dp.h

---
+++ project2/dp.h
@@ -0,0 +1,17 @@
+/* Project: #2 Dynamic Programming
+ *
+ * Team RADZ:
+ *  Rittie Chuaprasert (chuaprar)
+ *  Austin Dubina (weila)
+ *  Darren Baker (bakerdar)
+ *  Zac Knudson (knudsoza)
+ * 
+ * CS 325-400
+ * Winter 2014
+ * 
+*/
+
+#include <stdio.h>
+
+int dp_maxsubarray(int a_size, int a[]);
+
