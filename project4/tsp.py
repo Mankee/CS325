123127baddcd877180d81b31c368f4be1383d0b8
Zac <Zac520@aol.com>
2014-03-01 15:54:45 -0500

    This just illustrates that we cannot do a simple array for the city adjacency. If you run it, it gives "memory error" for the third example. Works for the second one.

--- project4/tsp.py
+++ project4/tsp.py
@@ -9,56 +9,67 @@ import sys, getopt ,math
 cityList = []
 filename = ''
 
+
+def getCityDistance(cityList, arrayIndex1, arrayIndex2):
+    cityDistance = math.sqrt((math.pow((cityList[arrayIndex1][1] - cityList[arrayIndex2][1]), 2)) +
+                             (math.pow((cityList[arrayIndex1][2] - cityList[arrayIndex2][2]), 2)))
+    cityDistance = round(cityDistance, 0) #round to the nearest integer. For 0.1, put a one, then for 0.01 put 2, etc. negative values work in the other direction
+    return cityDistance
+
+
 def find_tour():
-	global filename
-	global cityList
-	fin = open(filename, "r")
-	char = ""
-	str = ""
-	cityPieces = []
+    global filename
+    global cityList
+    fin = open(filename, "r")
+    char = ""
+    str = ""
+    cityPieces = []
 
-	for word in fin.read().split(): #first we split into giant array of words
-		cityPieces.append(word)
+    for word in fin.read().split(): #first we split into giant array of words
+        cityPieces.append(word)
 
-	fin.close() #close the file, we are done with it
+    fin.close() #close the file, we are done with it
 
-	for x in range(0, len(cityPieces)-2, 3): #now we sort into a 2d array with id, x, y at each index
-		cityList.append((int(cityPieces[x]), int(cityPieces[x+1]), int(cityPieces[x+2])))
+    for x in range(0, len(cityPieces)-2, 3): #now we sort into a 2d array with id, x, y at each index
+        cityList.append((int(cityPieces[x]), int(cityPieces[x+1]), int(cityPieces[x+2])))
 
-	del cityPieces[:] #delete from start to finish on the cityPieces
+    del cityPieces[:] #delete from start to finish on the cityPieces
 
-	#######################Now we start to play with the distances here#################################
+#######################Now we start to play with the distances here#################################
 
-	cityDistance = math.sqrt(math.pow((cityList[0][1] - cityList[1][1]),2) +
-					    	  math.pow((cityList[0][2] - cityList[1][2]),2))
 
-	cityDistance = round(cityDistance, 0) #round to the nearest integer. For 0.1, put a one, then for 0.01 put 2, etc. negative values work in the other direction
+    cityAdjacency = []
+    for x in range(len(cityList) -1):
+        for y in range(x+1, len(cityList)):
+            cityAdjacency.append((x,y,getCityDistance(cityList, x, y)))
 
-	# write output to file
-	filename = filename + '.tour' 
-	
-	fout = open(filename,'w')
-	fout.write("%.0f\n" % cityDistance) 
-	for city in cityList:
-		fout.write("{0:} {1:} {2:}\n".format(city[0], city[1], city[2]))
-	
-	fout.close()
+
+    print len(cityAdjacency)
+# write output to file
+    filename = filename + '.tour'
+
+    fout = open(filename, 'w')
+    fout.write("%.0f\n" % cityDistance)
+    for city in cityList:
+        fout.write("{0:} {1:} {2:}\n".format(city[0], city[1], city[2]))
+    fout.close()
+
 
 def main(argv):
-	global filename
+    global filename
 
-	opts, args = getopt.getopt(argv, "hf:", ["filename="])	
+    opts, args = getopt.getopt(argv, "hf:", ["filename="])
 
-	for o, a in opts:
-		if o == '-h':
-			print 'usage: tsp.py -f <filename>'
-			sys.exit()
-		elif o in ("-f", "--filename"):
-			filename = a
-		else:
-			assert False, "unhandled option"	
+    for o, a in opts:
+        if o == '-h':
+            print 'usage: tsp.py -f <filename>'
+            sys.exit()
+        elif o in ("-f", "--filename"):
+            filename = a
+        else:
+            assert False, "unhandled option"
 
-	find_tour()
+    find_tour()
 
 if __name__ == '__main__':
-	main(sys.argv[1:])
+    main(sys.argv[1:])
