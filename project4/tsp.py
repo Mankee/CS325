d17c0077235c58722d90faae1edca75b9bf7d439
BAKERDAR <bakerdar@onid.oregonstate.edu>
2014-03-04 14:16:35 -0800

    Merged Rittie's file, added signal handler

--- project4/tsp.py
+++ project4/tsp.py
@@ -9,7 +9,7 @@
 # CS 325-400
 # Winter 2014
 
-import sys, getopt, math
+import sys, getopt, math, signal, atexit, time
 
 cityList = []
 cityCount = 0
@@ -17,6 +17,12 @@ visited = []
 filename = ''
 tourLength = 0
 startCity = 15
+intervals = 10
+
+# The signal handler. On receiving sigterm, it writes
+# the latest result to the file.
+def sig_term():
+	output()
 
 #function to calculate the Euclidean distance between two cities
 def calc_dist(x1,y1,x2,y2):
@@ -44,24 +50,41 @@ def find_short_path(fromCity):
 	
 	return tourCity
 
+# function to output the visited tour file
+def output()
+	global visited
+	global tourLength
+	global filename
+	fout = 0
+	
+	filename += '.tour' 
+
+	fout = open(filename,'w')
+	fout.write("%.0f\n" % tourLength)
+	for city in visited:
+		fout.write("{0}\n".format(city))
+	
+	fout.close()
+
 # basic routine to optimize tour length by cycling through a number of 2-opt swaps and array reversals
 def optimize_tour():
 	global visited
+	tour_size = len(visited)
 
 	visited.reverse()
 
 	for cycle in range(10):
 		#print "------------------------- CYCLE", cycle, "------------------------------"
-		for i in range(2, cityCount-1):
-			for j in range(i, cityCount-1):
+		for i in range(2, tour_size-1):
+			for j in range(i, tour_size-1):
 				opt_swap(visited[i-2], visited[i-1], visited[j], visited[j+1])
 
 	visited.reverse()
 
 	for cycle in range(10):
 		#print "------------------------- CYCLE", cycle, "------------------------------"
-		for i in range(2, cityCount-1):
-			for j in range(i, cityCount-1):
+		for i in range(2, tour_size-1):
+			for j in range(i, tour_size-1):
 				opt_swap(visited[i-2], visited[i-1], visited[j], visited[j+1])
 
 
@@ -74,7 +97,7 @@ def tour_distance_check():
 	cumDistance = 0
 
 	# distance from starting city to all other cities
-	for x in range(1, cityCount):
+	for x in range(1, len(visited)):
 		thisDistance = calc_dist(cityList[visited[x-1]][0], cityList[visited[x-1]][1], cityList[visited[x]][0], cityList[visited[x]][1])
 		cumDistance += thisDistance
 
@@ -82,7 +105,7 @@ def tour_distance_check():
 	thisDistance = calc_dist(cityList[visited[x]][0], cityList[visited[x]][1], cityList[visited[0]][0], cityList[visited[0]][1])
 	cumDistance += thisDistance
 
-	print "#cities:", cityCount, "Length:", cumDistance
+	print "#cities:", len(visited), "Length:", cumDistance
 	tourLength = cumDistance
 
 # calculates the tour length given the current visited[] array.  Outputs city count and tour length.
@@ -95,13 +118,13 @@ def tour_distance_check_detail():
 	cumDistance = 0
 
 	# check figures: cities and main program tourLength
-	print "\n#cities:", cityCount, "Length:", tourLength
+	print "\n#cities:", len(visited), "Length:", tourLength
 
 	# first city - this is the tour starting point
 	print 0, cityList[visited[0]][0], cityList[visited[0]][1], cityList[visited[0]][2]
 
 	# distance from starting city to all other cities
-	for x in range(1, cityCount):
+	for x in range(1, len(visited)):
 		thisDistance = calc_dist(cityList[visited[x-1]][1], cityList[visited[x-1]][2], cityList[visited[x]][1], cityList[visited[x]][2])
 		cumDistance += thisDistance
 		print x, cityList[visited[x]][0], cityList[visited[x]][1], cityList[visited[x]][2], "dist:", thisDistance, "cum dist:", cumDistance
@@ -111,7 +134,7 @@ def tour_distance_check_detail():
 	cumDistance += thisDistance
 	print 0, cityList[visited[0]][0], cityList[visited[0]][1], cityList[visited[0]][2], "dist:", thisDistance, "cum dist:", cumDistance
 
-	print "#cities:", cityCount, "Length:", cumDistance
+	print "#cities:", len(visited), "Length:", cumDistance
 	tourLength = cumDistance
 
 
@@ -150,9 +173,19 @@ def find_tour():
 	global visited
 	global tourLength
 	global startCity
-	fout = 0
+	global intervals
 	distance = 0
 	tourCity = 0
+	x_max = 0
+	y_max = 0
+	x_min = 100000000
+	y_min = 100000000
+	x_step = 0
+	y_step = 0
+	x_idx = 0
+	y_idx = 0	
+	
+	mini_vis = [[0 for x in xrange(intervals*intervals)] for x in xrange(1000)]
 
 	with open(filename) as f:
 		for line in f:
@@ -161,6 +194,101 @@ def find_tour():
 	#get the count of cities
 	cityCount = len(cityList)
 
+	# get x and y max and min
+	for i in range(cityCount):
+		if (cityList[i][0] > x_max):
+			x_max = cityList[i][0]
+		elif (cityList[i][0] < x_min):
+			x_min = cityList[i][0]
+
+		if (cityList[i][1] > y_max):
+			y_max = cityList[i][1]
+		elif (cityList[i][1]) < y_min:
+			y_min = cityList[i][1]
+		#visited.append(i)
+
+	print x_min, x_max, y_min, y_max, "cityCount:", cityCount
+	x_step = (x_max - x_min)/intervals + 1
+	y_step = (y_max - y_min)/intervals + 1
+	
+	for i in range(intervals):
+		for j in range(intervals):
+			mini_vis[i*intervals + j] = []
+
+	for i in range(cityCount):
+		x_idx = (cityList[i][0] - x_min) / x_step
+		y_idx = (cityList[i][1] - y_min) / y_step
+		#print x_idx, y_idx		
+		mini_vis[x_idx*intervals + y_idx].append(i)
+
+	for i in range(intervals):
+		for j in range(intervals):
+			print i, j, "count:", len(mini_vis[i*intervals + j])
+			#print mini_vis[i*intervals + j]
+
+	# determine the order of the tour as it hops mini-segments
+	tour_order = []
+	curr_idx = 0
+	for i in range(intervals):
+		tour_order += [i*intervals]
+
+	curr_idx = intervals * (intervals-1)
+	for i in range(intervals-1):
+		curr_idx += 1
+		tour_order += [curr_idx]
+
+	curr_idx -= intervals
+
+	for loop in range(intervals/2 - 1):
+		tour_order += [curr_idx]
+		for i in range(intervals-2):
+			curr_idx -= intervals
+			tour_order += [curr_idx]
+
+		curr_idx -= 1
+		tour_order += [curr_idx]
+		for i in range(intervals-2):
+			curr_idx += intervals
+			tour_order += [curr_idx]
+
+		curr_idx -= 1
+		print curr_idx, tour_order
+
+	tour_order += [curr_idx]
+	for i in range(intervals-2):
+		curr_idx -= intervals
+		tour_order += [curr_idx]
+
+	"""
+	tour_order = [0,10,20,30,40,50,60,70,80,90,91,92,93,94,95,96,97,98,99]
+	tour_order += [89,79,69,59,49,39,29,19,9]
+	tour_order += [8,18,28,38,48,58,68,78,88]
+	tour_order += [87,77,67,57,47,37,27,17,7]
+	tour_order += [6,16,26,36,46,56,66,76,86]
+	tour_order += [85,75,65,55,45,35,25,15,5]
+	tour_order += [4,14,24,34,44,54,64,74,84]
+	tour_order += [83,73,63,53,43,33,23,13,3]
+	tour_order += [2,12,22,32,42,52,62,72,82]
+	tour_order += [81,71,61,51,41,31,21,11,1]
+	"""
+	print tour_order
+
+	# optimize the mini-arrays
+	for i in range(intervals):
+		for j in range(intervals):
+			if (len(mini_vis[i]) > 3):
+				visited = mini_vis[i*intervals + j]
+				print i, j, len(mini_vis[i*intervals + j]), visited
+				optimize_tour()
+				mini_vis[i*intervals + j] = visited
+
+	visited = []
+	for i in range(len(tour_order)):
+		visited += mini_vis[tour_order[i]]
+
+	print visited
+
+	"""
 	#go on a tour {note: at this stage, starting city 15 is optimal for sample1; city 165 is optimal for sample2}
 	if (startCity > cityCount):
 		startCity = 0
@@ -169,6 +297,7 @@ def find_tour():
 	for c in range(cityCount - 1):
 		tourCity = find_short_path(tourCity)
 		visited.append(tourCity)
+	"""
 
 	#-----added by Rittie----------------------------------------------
 	#for x in range(cityCount):
@@ -180,20 +309,16 @@ def find_tour():
 	#------------------------------------------------------------------
 
 	# write output to file
-	filename += '.tour' 
-
-	fout = open(filename,'w')
-	fout.write("%.0f\n" % tourLength)
-	for city in visited:
-		fout.write("{0}\n".format(city))
-	
-	fout.close()
+	output()
 
 def main(argv):
 	global filename
-
+	
+	# Register signal handler
+    signal.signal(signal.SIGTERM, sig_term)
+	
 	opts, args = getopt.getopt(argv, "hf:", ["filename="])	
-
+	
 	for o, a in opts:
 		if o == '-h':
 			print 'usage: tsp.py -f <filename>'
